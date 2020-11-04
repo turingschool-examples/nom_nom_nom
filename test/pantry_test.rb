ec1c84776b336ce4aaeb03b30ff6874f67308fb3
Catherine Dean <catherinemdean15@gmail.com>
2020-11-04 13:58:56 -0700

    add test_enough_ingredients_for? to Recipe test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -31,4 +31,20 @@ class PantryTest < Minitest::Test
     @pantry.restock(@ingredient2, 7)
     assert_equal 7, @pantry.stock_check(@ingredient2)
   end
+
+  def test_enough_ingredients_for?
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+
+    @pantry.restock(@ingredient2, 7)
+
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+
+    @pantry.restock(@ingredient2, 1)
+
+    assert_equal true, @pantry.enough_ingredients_for?(@recipe1)
+  end
+  
 end
