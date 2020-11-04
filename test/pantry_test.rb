cef769aa1aa6a926421c930e19ead0467e26273b
Dominic Padula <domopadula2192@gmail.com>
2020-11-04 13:25:20 -0700

    Add all tests that stock starts at 0 and restock adds to stock_check

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -21,4 +21,21 @@ class PantryTest < Minitest::Test
   def test_it_exists
     assert_instance_of Pantry, @pantry
   end
+
+  def test_stock_pantry_starts_as_empty_hash
+    expected = {}
+    assert_equal expected, @pantry.stock
+  end
+
+  def test_stock_starts_at_0
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+
+  def test_restock_adds_to_stock_count
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    @pantry.restock(@ingredient2, 7)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+    assert_equal 7, @pantry.stock_check(@ingredient2)
+  end 
 end
