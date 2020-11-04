93b35dd1edae1d5dcefe9b6f101c5999970c1373
Dominic Padula <domopadula2192@gmail.com>
2020-11-04 13:47:36 -0700

    add tests for add_ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -10,7 +10,10 @@ class RecipeTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_exists_and_has_attributes
@@ -18,5 +21,13 @@ class RecipeTest < Minitest::Test
     assert_equal "Mac and Cheese", @recipe1.name
   end
 
+  def test_we_can_add_ingredients_to_ingredients_required
+    expected = {}
+    assert_equal expected, @recipe1.ingredients_required
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    assert_equal [@ingredient1 => 6, @ingredient2=> 8], @recipe1.ingredients_required
+  end
 
 end
