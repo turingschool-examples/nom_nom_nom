bddd126bdf96789d7a64e7605680e14c4662fd68
Dominic Padula <domopadula2192@gmail.com>
2020-11-04 14:02:16 -0700

    Fix test for ingredients_required and add test for ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -27,7 +27,17 @@ class RecipeTest < Minitest::Test
     @recipe1.add_ingredient(@ingredient1, 2)
     @recipe1.add_ingredient(@ingredient1, 4)
     @recipe1.add_ingredient(@ingredient2, 8)
-    assert_equal [@ingredient1 => 6, @ingredient2=> 8], @recipe1.ingredients_required
+    expected = {@ingredient1 => 6, @ingredient2 => 8}
+
+    assert_equal expected, @recipe1.ingredients_required
+  end
+
+  def test_ingredients_is_an_array_we_add_to
+    assert_equal [], @recipe1.ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients 
   end
 
 end
