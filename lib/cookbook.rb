d73e3cedd5047ecbbee87aa2d516dacdb6425bd0
Catherine Dean <catherinemdean15@gmail.com>
2020-11-04 13:53:14 -0700

    add ingredients method to CookBook class

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -8,5 +8,13 @@ attr_reader :recipes
   def add_recipe(recipe)
     @recipes << recipe
   end
-  
+
+  def ingredients
+    @recipes.flat_map do |recipe|
+      recipe.ingredients.map do |ingredient|
+        ingredient.name
+      end
+    end.uniq
+  end
+
 end
--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -11,6 +11,8 @@ class CookBookTest < Minitest::Test
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
     @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
     @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe2.add_ingredient(@ingredient1, 2)
