ffdfa1a8dee1a5f25b4ff04f95d2e51604204380
Cydnee Owens <cks410@yahoo.com>
2020-11-04 13:10:55 -0700

    Add initial test pantry

---
+++ test/pantry_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Pantry, @pantry
+  end
+end 
