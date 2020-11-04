bbf564726b85d880e6eebdfcbfe48eb027a31c95
Grayson Myers <graymyers10@gmail.com>
2020-11-04 13:23:29 -0700

    Add test for restock method of Pantry class and mocks for Ingredient objects

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,15 +1,30 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "mocha/minitest"
 require "./lib/pantry"
 
 class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
+    @ingredient1 = mock()
+    @ingredient1.stubs("name").returns("Cheese")
+    @ingredient1.stubs("unit").returns("oz")
+    @ingredient1.stubs("calories").returns("50")
+
+    @ingredient2 = mock()
+    @ingredient2.stubs("name").returns("Macaroni")
+    @ingredient2.stubs("unit").returns("oz")
+    @ingredient2.stubs("calories").returns("200")
   end
 
   def test_it_exists_and_has_attribute
     assert_instance_of Pantry, @pantry
 
     assert_equal Hash.new, @pantry.stock
+  end
+
+  def test_it_restocks
+    assert_equal 5, @pantry.restock(@ingredient1, 5)
+    assert_equal 7, @pantry.restock(@ingredient1, 7)
   end
 end
