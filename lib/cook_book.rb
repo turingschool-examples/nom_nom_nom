d64319fd12920938fb8749744fd9deb91cc2ddbd
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 14:00:30 -0700

    Add ingredients method, test passing

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -9,4 +9,11 @@ class CookBook
     @recipes.push(recipe)
   end
 
+  def ingredients
+    @recipes.flat_map do |recipe|
+      recipe.ingredients_required.flat_map do |ingredient, qty|
+        ingredient.name
+      end
+    end.uniq
+  end
 end
