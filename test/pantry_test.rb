e73311c585f11dd25bbad1d6e16c6ae0c30ad6eb
Liam Cusack <LCusack8108@gmail.com>
2020-11-04 13:13:15 -0700

    Add Pantry test_it_exists and test pass

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,14 @@ require './lib/ingredient'
 require "./lib/pantry"
 
 class PantryTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+
+    assert_instance_of Pantry, @pantry
+  end
 end
