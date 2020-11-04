112096f7c7eb346f13d951a0bae2d60fb1fb58d4
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 14:00:13 -0700

    Add test for ingredients method

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -6,12 +6,20 @@ require './lib/recipe'
 
 class CookBookTest < Minitest::Test
   def setup
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @cookbook = CookBook.new
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
-    @cookbook = CookBook.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
   end
+
   def test_it_exists_and_has_attributes
     assert_instance_of CookBook, @cookbook
   end
@@ -21,4 +29,14 @@ class CookBookTest < Minitest::Test
     @cookbook.add_recipe(@recipe2)
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
+
+  def test_it_can_return_ingredients
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], @cookbook.ingredients
+  end
 end
+
+
+
+# @cookbook.highest_calorie_meal
