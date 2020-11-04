1f0b03ce1252d114f37766c0a1d2372a79336631
Cydnee Owens <cks410@yahoo.com>
2020-11-04 13:23:50 -0700

    Add ingredients required test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -15,4 +15,18 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, @recipe1
     assert_equal "Mac and Cheese", @recipe1.name
   end
+
+  def test_it_can_find_ingredients_required 
+    expected = {}
+    assert_equal expected, @recipe1.ingredients_required
+   
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    expected = {@ingredient1 => 6, @ingredient2 => 8}
+    assert_equal expected, @recipe1.ingredients_required
+    
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
 end
