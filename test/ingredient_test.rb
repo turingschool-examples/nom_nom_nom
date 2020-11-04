c00b0bb0ad8e3d22c729d829b2001264e633e100
Yesi Meza <yesi.meza10@gmail.com>
2020-11-04 13:14:13 -0700

    created ingredients class and initialized

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -8,5 +8,8 @@ class IngredientTest < Minitest::Test
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
 
     assert_instance_of Ingredient, ingredient1
+    assert_equal "Cheese", ingredient1.name
+    assert_equal "oz", ingredient1.unit
+    assert_equal 50, ingredient1.calories
   end
 end
