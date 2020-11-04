071474abd13cdaa1635d2c0b223fed627cbc1d1e
Grayson Cunningham <gecun289@gmail.com>
2020-11-04 13:12:09 -0700

    Create ingredient class with initialize method and attr_readers

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -8,7 +8,7 @@ class IngredientTest < Minitest::Test
   end
 
   def test_it_exists_and_has_attributes
-    assert_instance_of Ingredient, @ingredient
+    assert_instance_of Ingredient, @ingredient1
     assert_equal "Cheese", @ingredient1.name
     assert_equal "oz", @ingredient1.unit
     assert_equal 50, @ingredient1.calories
