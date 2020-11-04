290121410d1d0e27da456d5ace705f678465b648
Robert Heath <robert.b.heathii@gmail.com>
2020-11-04 14:19:20 -0600

    Code stock and restock in Pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -6,4 +6,12 @@ class Pantry
   def initialize
     @stock = Hash.new(0)
   end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    @stock[ingredient] += amount
+  end
 end
