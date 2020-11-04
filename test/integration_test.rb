2ac1d38b88fd386a839a5d386dc8d8d2857c6f48
Sam Yeo <yeosamuel95@gmail.com>
2020-11-04 14:31:50 -0700

    Add test for summary

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -77,4 +77,30 @@ class IntegrationTest < MiniTest::Test
         assert_equal "11-04-2020", @cookbook.date
     end
 
+    def test_
+        @recipe1.add_ingredient(@ingredient1, 2)
+        @recipe1.add_ingredient(@ingredient2, 8)
+        @recipe2.add_ingredient(@ingredient3, 4)
+        @recipe2.add_ingredient(@ingredient4, 100)
+        @cookbook.add_recipe(@recipe1)
+        @cookbook.add_recipe(@recipe2)
+
+        expected = [
+            {:name => "Mac and Cheese", 
+                :details => 
+                    {:ingredients => 
+                        [{:ingredient => "Macaroni", :amount => "8 oz"}, 
+                         {:ingredient=>"Cheese", :amount=>"2 C"}],
+                    :total_calories=>440}
+            }, 
+            {:name=>"Burger", 
+                :details=>
+                    {:ingredients=>
+                        [{:ingredient=>"Ground Beef", :amount=>"4 oz"}, 
+                         {:ingredient=>"Bun", :amount=>"100 g"}],
+                    :total_calories=>500}
+            }
+        ]
+        assert_equal expected, @cookbook.summary
+    end
 end
