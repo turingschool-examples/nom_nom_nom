d1155a711370e74fd28e47081bf4d1439f9bfbbe
Joseph Budina <joebud3@gmail.com>
2020-11-04 14:42:36 -0700

    Add total calorie mehtod

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -17,4 +17,10 @@ class Recipe
       ingredient
     end.flatten
   end 
+
+  def total_calories
+    @ingredients_required.sum do |ingredient, quantity|
+      ingredient.calories * quantity
+    end
+  end
 end
