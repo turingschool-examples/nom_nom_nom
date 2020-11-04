c25cbaedd36fd97d2f369713aac1bb5ab0dde6f9
Adam Etzion <adametzion90@gmail.com>
2020-11-04 13:08:18 -0700

    Add ingredient tests

---
+++ test/ingredient_test.rb
@@ -0,0 +1,23 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class IngredientTest < Minitest::Test
+    def setup
+        @ingredient1 = Ingredient.new(
+            {
+                name: "Cheese",
+                unit: "oz",
+                calories: 50
+            }
+        )
+    end
+
+    def test_it_exists_and_has_attributes
+        assert_instance_of Ingredient, @ingredient1
+        assert_equal "Cheese", @ingredient1.name
+        assert_equal "oz", @ingredient1.unit
+        assert_equal 50, @ingredient1.calories
+    end
+end
