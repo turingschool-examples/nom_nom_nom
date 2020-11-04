694f329d91bffb8fc5f859b3c7c9d51fd754cc85
Aiden Mendez <aidenmendez@gmail.com>
2020-11-04 14:19:38 -0700

    Test cookbook class can return highest calorie meal

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -10,6 +10,18 @@ class CookbookTest < Minitest::Test
     @cookbook = Cookbook.new
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
   end
 
   def test_it_exists	
@@ -20,9 +32,13 @@ class CookbookTest < Minitest::Test
   def test_it_can_add_recipes
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
-    
+
     assert_equal "Invalid recipe!", @cookbook.add_recipe("McMuffin")
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
     
+  end
+
+  def test_highest_calorie_meal
+    assert_equal @recipe2, @cookbook.highest_calorie_meal
   end
 end
