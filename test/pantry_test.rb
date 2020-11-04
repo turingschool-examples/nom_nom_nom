45b8e509ceb6c912843f8a7be9605c57c9548f08
Sam Yeo <yeosamuel95@gmail.com>
2020-11-04 13:16:04 -0700

    Add test for restock ingredients

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -18,4 +18,11 @@ class PantryTest < MiniTest::Test
     def test_it_can_check_stock_for_ingredient
         assert_equal 0, @pantry.stock_check(@ingredient1)
     end
+
+    def test_it_can_restock_ingredients
+        @pantry.restock(@ingredient1, 5)
+        @pantry.restock(ingredient1, 10)
+        
+        assert_equal 15, @pantry.stock_check(@ingredient1)
+    end
 end
