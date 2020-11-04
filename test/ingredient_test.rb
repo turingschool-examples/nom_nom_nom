df54ba69a35ef0f8383a9cacb8fb4159e922fc3a
James Fox-Collis <jlfoxcollis@gmail.com>
2020-11-04 13:05:48 -0700

    tested Ingredient exists

---
+++ test/ingredient_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require 'pry'
+
+class IngredientTest < MiniTest::Test
+
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  end
+
+  def test_it_exists_and_has_attributes
+
+    assert_equal "Cheese", @ingredient1.name
+    assert_equal "oz", @ingredient1.unit
+    assert_equal 50, @ingredient1.calories
+  end
+end
