c73ebbc6703acc550a334cc91ba7bb4b34051282
Ely Hess <ely.hess@me.com>
2020-11-04 13:44:37 -0700

    Add ingredients method

---
+++ lib/cook_book.rb
@@ -0,0 +1,22 @@
+class CookBook
+  attr_reader :recipes
+
+  def initialize
+    @recipes = []
+  end
+
+  def add_recipe(recipe)
+    @recipes << recipe
+  end
+
+  def ingredients
+    all_ingredients = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |ingredient_required, quantity|
+        all_ingredients << ingredient_required.name 
+      end
+    end
+    all_ingredients.uniq
+  end
+
+end
