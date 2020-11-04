c4b0e00cd733250372e09abbca7ebd71207d6a11
Ely Hess <ely.hess@me.com>
2020-11-04 14:58:14 -0700

    incomplete enough_ingredients_for method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,26 +1,25 @@
 class Pantry
-  attr_reader :stock  # => nil
+  attr_reader :stock
 
   def initialize
     @stock = Hash.new {|hash, key| hash[key] = 0}
-  end                                              # => :initialize
+  end
 
   def stock_check(item)
     @stock[item]
-  end                    # => :stock_check
+  end
 
   def restock(item, quantity)
     @stock[item] += quantity
-  end                          # => :restock
+  end
 
   def enough_ingredients_for?(recipe)
     recipe.ingredients_required.all? do |ingredient, quantity|
-      require "pry"; binding.pry
       quantity >= stock_check(ingredient)
     end
-  end                                                           # => :enough_ingredients_for?
+  end
 
 
 
 
-end  # => :enough_ingredients_for?
+end
