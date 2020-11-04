285bbd7f474e3ccce70c05e68bfc8c3761c2cc88
Catherine Dean <catherinemdean15@gmail.com>
2020-11-04 14:04:06 -0700

    revise test_enough_ingredients_for? to include recipe1

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
 
@@ -33,18 +34,23 @@ class PantryTest < Minitest::Test
   end
 
   def test_enough_ingredients_for?
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe1.add_ingredient(@ingredient1, 2)
+    recipe1.add_ingredient(@ingredient2, 8)
+
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
 
-    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    assert_equal false, @pantry.enough_ingredients_for?(recipe1)
 
     @pantry.restock(@ingredient2, 7)
 
-    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    assert_equal false, @pantry.enough_ingredients_for?(recipe1)
 
     @pantry.restock(@ingredient2, 1)
 
     assert_equal true, @pantry.enough_ingredients_for?(@recipe1)
   end
-  
+
 end
