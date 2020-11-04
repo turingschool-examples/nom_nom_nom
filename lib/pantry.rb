57d82449f047fcd32ff5716670f84fde1b2c3451
Max Ribbans <ribbans.max@gmail.com>
2020-11-04 15:16:24 -0500

    Add stock methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -4,4 +4,20 @@ class Pantry
   def initialize
     @stock = {}
   end
+
+  def stock_check(ingredient)
+    if stock[ingredient]
+      stock[ingredient]
+    else
+      0
+    end
+  end
+
+  def restock(ingredient, amount)
+    if stock[ingredient]
+      stock[ingredient] += amount
+    else
+      stock[ingredient] = amount
+    end
+  end
 end
