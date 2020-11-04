433ca9ab08d41c9ab1511046f534353b1bd988cd
Max Ribbans <ribbans.max@gmail.com>
2020-11-04 15:27:43 -0500

    Add ingredients method

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -30,4 +30,16 @@ class RecipeTest < Minitest::Test
 
     assert_equal expected, recipe1.ingredients_required
   end
+
+  def test_it_can_return_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
+  end
 end
