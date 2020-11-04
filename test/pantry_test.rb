b3fce8f8753776f2ceed17f115dc6577c9cb8224
Catherine Dean <catherinemdean15@gmail.com>
2020-11-04 13:11:05 -0700

    add test_it_calls_stock test to Pantry test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -15,4 +15,8 @@ class PantryTest < Minitest::Test
     assert_instance_of Pantry, @pantry
   end
 
+  def test_it_calls_stock
+    expected = {}
+    assert_equal expected, @pantry.stock
+  end
 end
