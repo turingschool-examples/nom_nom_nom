c63cbf6c511f9353a9b80564713e25ebedcc8847
Sam Yeo <yeosamuel95@gmail.com>
2020-11-04 13:44:07 -0700

    Add test for total_calories

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -17,4 +17,15 @@ class IntegrationTest < MiniTest::Test
         @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     end
 
+    def test_it_can_get_total_calories_from_recipe
+        @recipe1.add_ingredient(@ingredient1, 2)
+        @recipe1.add_ingredient(@ingredient2, 8)
+        @recipe2.add_ingredient(@ingredient1, 2)
+        @recipe2.add_ingredient(@ingredient3, 4)
+        @recipe2.add_ingredient(@ingredient4, 1)
+
+        assert_equal 440, @recipe1.total_calories
+        assert_equal 675, @recipe2.total_calories
+    end
+
 end
