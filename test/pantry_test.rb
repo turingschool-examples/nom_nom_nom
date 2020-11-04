0665611d944500a1bd5f8edd6e53b548bd87154d
Aiden Mendez <aidenmendez@gmail.com>
2020-11-04 13:15:12 -0700

    Test stock check method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,14 +1,22 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/pantry'
+require './lib/ingredient'
 
 class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
+
   def test_it_exists
     stock_result = {}
     assert_instance_of Pantry, @pantry 
     assert_equal stock_result, @pantry.stock
+  end
+
+  def test_stock_check
+    assert_equal 0, @pantry.stock_check(@ingredient1)
   end
 end
