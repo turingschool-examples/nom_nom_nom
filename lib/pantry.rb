ecf5ddaf1ec81ae7196c78f5698b40d29ec99963
Aiden Mendez <aidenmendez@gmail.com>
2020-11-04 13:18:23 -0700

    Create stock_check method in pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,6 +1,15 @@
 class Pantry
   attr_reader :stock
+
   def initialize
     @stock = {}
+  end
+
+  def stock_check(ingredient)
+    if stock.key?(ingredient)
+      stock[ingredient]
+    else
+      0
+    end
   end
 end
