38f1d4710314a402bb7c3fad5fe1b49fc4113624
Robert Heath <robert.b.heathii@gmail.com>
2020-11-04 15:11:18 -0600

    Test cookbook ingredients

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -5,17 +5,61 @@ require './lib/cookbook'
 class CookBookTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
-    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-    recipe1 = Recipe.new("Mac and Cheese")
-    recipe2 = Recipe.new("Cheese Burger")
-
     cookbook = CookBook.new
     assert_instance_of CookBook, cookbook
     assert_equal [], cookbook.recipes
+  end
+
+  def test_add_recipe
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook = CookBook.new
 
     cookbook.add_recipe(recipe1)
     cookbook.add_recipe(recipe2)
+
     assert_equal [recipe1, recipe2], cookbook.recipes
+  end
+
+  def test_ingredients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook = CookBook.new
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, cookbook.ingredients
+  end
+
+  def test_highest_calorie_meal
+    skip
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    assert_equal recipe2, cookbook.highest_calorie_meal
   end
 end
