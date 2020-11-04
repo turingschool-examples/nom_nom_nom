6f45b3248910336daefbd97431fbe6170cb68cb1
Max Ribbans <ribbans.max@gmail.com>
2020-11-04 15:29:18 -0500

    Create file

---
+++ test/cookbook_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cookbook'
+
+class CookBookTest < Minitest::Test
+
+end
