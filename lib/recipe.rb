275561c14a2c1feab1036fa7149dfc351dde7779
Dominic Padula <domopadula2192@gmail.com>
2020-11-04 14:03:11 -0700

    create add_ingredient which manipulates both hash and array

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,11 +1,14 @@
 class Recipe
-  attr_reader:name, :ingredients_required
+  attr_reader:name, :ingredients_required, :ingredients
   def initialize(name)
     @name = name
     @ingredients_required = Hash.new(0)
+    @ingredients = []
   end
 
   def add_ingredient(ingredient, amount)
     @ingredients_required[ingredient] += amount
-  end 
+    @ingredients << ingredient
+    @ingredients.uniq!
+  end
 end
