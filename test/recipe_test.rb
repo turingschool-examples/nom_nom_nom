dd421b3df2ee3ba84dcc6f834b7c65a6d5170534
Michael Foy <foymikek@gmail.com>
2020-11-04 13:34:20 -0700

    Add test_recipe_can_list_ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -17,7 +17,6 @@ class RecipeTest < Minitest::Test
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     recipe1 = Recipe.new("Mac and Cheese")
-    assert_equal "Mac and Cheese", recipe1.name
 
     expected = {}
     assert_equal expected, recipe1.ingredients_required
@@ -28,6 +27,17 @@ class RecipeTest < Minitest::Test
 
     expected = {ingredient1 => 6, ingredient2 => 8}
     assert_equal expected, recipe1.ingredients_required
+  end
+
+  def test_recipe_can_list_ingredients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
   end
 
 end
