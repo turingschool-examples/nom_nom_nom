9d618688f5d02252908ad869b011c91bdc07b67c
Max Ribbans <ribbans.max@gmail.com>
2020-11-04 15:32:52 -0500

    Add initialize and add_recipes method

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -6,5 +6,14 @@ require './lib/recipe'
 require './lib/cookbook'
 
 class CookBookTest < Minitest::Test
+  def test_it_exists_and_can_add_and_have_recipes
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
 
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end
