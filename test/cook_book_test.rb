93258da4d4fb99cb63763629a04d29d9c686db63
Michael Foy <foymikek@gmail.com>
2020-11-04 13:48:23 -0700

    Add test_cook_book_can_list_ingredients

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -25,4 +25,24 @@ class CookBookTest < Minitest::Test
 
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
+
+  def test_cook_book_can_list_ingredients
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, cookbook.ingredients
+  end
 end
