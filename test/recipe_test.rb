84c98908428d8ced606e23843b55c485d88e420f
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 13:41:49 -0700

    Add test case for total_calories method

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,7 +5,12 @@ require './lib/ingredient'
 
 class RecipeTest < Minitest::Test
   def setup
-
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_exists_and_has_attributes
@@ -27,22 +32,13 @@ class RecipeTest < Minitest::Test
     assert_equal [ingredient1, ingredient2], recipe1.ingredients
   end
 
-
+  def test_it_can_calculate_recipe_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, @recipe2.total_calories
+  end
 end
-#
-#
-# @pantry = Pantry.new
-# @cookbook = CookBook.new
-# @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-# @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-# @recipe1 = Recipe.new("Mac and Cheese")
-# @recipe1.add_ingredient(@ingredient1, 2)
-# @recipe1.add_ingredient(@ingredient2, 8)
-# @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
-# @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
-# @recipe2 = Recipe.new("Cheese Burger")
-# @recipe2.add_ingredient(@ingredient1, 2)
-# @recipe2.add_ingredient(@ingredient3, 4)
-# @recipe2.add_ingredient(@ingredient4, 1)
-# assert_equal 440, @recipe1.total_calories
-# assert_equal 675, @recipe2.total_calories
