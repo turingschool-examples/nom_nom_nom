1193481cbb2eecafe47b47793df8b3523b909315
Sam Yeo <yeosamuel95@gmail.com>
2020-11-04 13:05:17 -0700

    Setup basic files for Iteration 1, add first test case for Ingredient

---
+++ test/ingredient_test.rb
@@ -0,0 +1,17 @@
+require './lib/ingredient'
+require './lib/pantry'
+require 'minitest/autorun'
+
+class IngredientTest < MiniTest::Test
+
+    def setup
+        @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50}) 
+    end
+
+    def test_it_can_instantiate
+        assert_equal "Cheese", @ingredient1.name
+        assert_equal "oz", @ingredient1.unit
+        assert_equal 50, @ingredient1.calories
+    end
+
+end
