1c4980fb4c96464d32be2f6d09e7ca14bbbf1041
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 13:11:14 -0700

    Add method stock_check, passing test

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -2,6 +2,10 @@ class Pantry
   attr_reader :stock
 
   def initialize
-    @stock = {}
+    @stock = Hash.new { |stock, ingredient| stock[ingredient] = 0 }
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
   end
 end
