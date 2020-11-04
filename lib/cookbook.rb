b4c9f57e8349d15789bdb00e17ec4387f4cc510d
Robert Heath <robert.b.heathii@gmail.com>
2020-11-04 15:11:38 -0600

    Code ingredients in cookbook

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -10,4 +10,12 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    @recipes.flat_map do |recipe|
+      recipe.ingredients.map do |ingredient|
+        ingredient.name
+      end
+    end.uniq
+  end
 end
