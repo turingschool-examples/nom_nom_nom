952797160e06f280fdac31a2ad6513b301e33676
ninesky00 <jiangzho@gmail.com>
2020-11-04 14:46:58 -0600

    Add test to calculate total calories from ingredients in recipe class.

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -7,7 +7,10 @@ class Test < MiniTest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @ecipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_attributes_are_instantiated
@@ -23,5 +26,15 @@ class Test < MiniTest::Test
     expected = {@ingredient1 => 6, @ingredient2 => 8}
     assert_equal expected, @recipe1.ingredients_required
     assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
+
+  def test_can_sum_total_calories_from_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, @recipe2.total_calories
   end
 end
