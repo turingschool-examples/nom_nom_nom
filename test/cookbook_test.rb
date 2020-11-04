7609f71a3b0992fcfb2f5634975703dc1ded867a
Aiden Mendez <aidenmendez@gmail.com>
2020-11-04 13:54:56 -0700

    Test cookbook can add recipe

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -1,15 +1,26 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/cookbook'
+require './lib/recipe'
 require './lib/pantry'
 require './lib/ingredient'
 
 class CookbookTest < Minitest::Test
   def setup
     @cookbook = Cookbook.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_exists	
-    assert_instance of Cookbook, @cookbook
+    assert_instance_of Cookbook, @cookbook
+    assert_equal [], @cookbook.recipes
+  end
+
+  def test_it_can_add_recipes
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
 end
