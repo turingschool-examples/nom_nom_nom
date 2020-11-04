16d5091a06cef00ebce5e1a222cbfbbe459dcdf0
Michael Foy <foymikek@gmail.com>
2020-11-04 13:16:17 -0700

    Add stock_check(item_obj) method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,7 +1,17 @@
 class Pantry
   attr_reader :stock
-  
+
   def initialize
     @stock = {}
   end
+
+  def stock_check(item_obj)
+    if @stock[item_obj].nil?
+      0
+    else
+      @stock[item_obj]
+    end
+  end
+
+  
 end
