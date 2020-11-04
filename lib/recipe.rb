e66f6626999746c58f3f1b8103ccadcb2422ce33
Catherine Dean <catherinemdean15@gmail.com>
2020-11-04 13:26:03 -0700

    add add_ingredient method to Recipe class

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,15 @@
 class Recipe
-attr_reader :name
+attr_reader :name,
+            :ingredients_required
 
   def initialize(name)
     @name = name
+    @ingredients_required = Hash.new
+  end
+
+  def add_ingredient(ingredient, amount)
+    (@ingredients_required[ingredient] += amount if @ingredients_required[ingredient]) ||
+    (@ingredients_required[ingredient] = amount) 
   end
 
 end
