f95e143b9c332f6202e43fbe016049a76d3350e6
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 14:16:42 -0700

    Add test for enough_ingredients_for method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,25 +2,53 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/pantry'
 require './lib/ingredient'
+require './lib/cook_book'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
   def setup
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     @pantry = Pantry.new
+    @cookbook = CookBook.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
   end
 
   def test_it_exists_and_has_attributes
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    pantry = Pantry.new
     expected = {}
-    assert_equal expected, @pantry.stock
+    assert_equal expected, pantry.stock
   end
 
   def test_it_can_check_stock_of_ingredient_and_restock
-    assert_equal 0, @pantry.stock_check(@ingredient1)
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check(ingredient1)
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    assert_equal 15, pantry.stock_check(ingredient1)
+    pantry.restock(ingredient2, 7)
+    assert_equal 7, pantry.stock_check(ingredient2)
+  end
+
+  def test_enough_ingredients_for
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
-    assert_equal 15, @pantry.stock_check(@ingredient1)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
     @pantry.restock(@ingredient2, 7)
-    assert_equal 7, @pantry.stock_check(@ingredient2)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    @pantry.restock(@ingredient2, 1)
+    assert_equal true, @pantry.enough_ingredients_for?(@recipe1)
   end
 end
