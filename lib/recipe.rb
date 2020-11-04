8c5416d79a803c702451cc7209c8c9d78dedaac7
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 13:23:51 -0700

    Add add_ingredient method, passing test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,4 +6,8 @@ class Recipe
     @name = name
     @ingredients_required = Hash.new { |ing_req, ing| ing_req[ing] = 0 }
   end
+
+  def add_ingredient(ingredient, qty)
+    @ingredients_required[ingredient] += qty
+  end
 end
