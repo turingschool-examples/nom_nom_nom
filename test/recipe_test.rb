40da615c9529c9f6e60d1eefcc9779b4671a71d2
Robert Heath <robert.b.heathii@gmail.com>
2020-11-04 14:37:46 -0600

    Test for add of ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,11 +5,26 @@ require './lib/recipe'
 class RecipeTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
-    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+   ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
    recipe1 = Recipe.new("Mac and Cheese")
    assert_instance_of Recipe, recipe1
    assert_equal "Mac and Cheese", recipe1.name
    assert_equal ({}), recipe1.ingredients_required
+  end
+
+  def test_it_can_add_ingredients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    expected = {ingredient1 => 6, ingredient2 => 8}
+    assert_equal expected, recipe1.ingredients_required
+    expected2 = [ingredient1, ingredient2]
+    assert_equal expected2, recipe1.ingredients
   end
 end
