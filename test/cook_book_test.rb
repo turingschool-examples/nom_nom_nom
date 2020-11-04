a6f8f199300640df3b369555c9806cde1c87581b
Michael Foy <foymikek@gmail.com>
2020-11-04 14:40:59 -0700

    Add test_cook_book_can_list_recipies_from_higest_to_lowwest_calories

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -64,4 +64,48 @@ class CookBookTest < Minitest::Test
 
     assert_equal recipe2, cookbook.highest_calorie_meal
   end
+
+  def test_cook_book_can_list_recipies_from_higest_to_lowwest_calories
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+    recipe2 = Recipe.new("Burger")
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 100)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.ordered_caloric_recipies
+  end
+
+  def test_cook_book_calories_summary
+    skip
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+    recipe2 = Recipe.new("Burger")
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 100)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    expected = [
+    {:name=>"Mac and Cheese", :details => {:ingredients => [{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}],
+      :total_calories=>440}},
+      {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"},
+         {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}
+         }]
+    assert_equal expected, cookbook.summary
+  end
+
 end
