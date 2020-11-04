49e5f4f5e1a6a27d2edc9c3e8060178758b4a4df
Grayson Myers <graymyers10@gmail.com>
2020-11-04 13:57:32 -0700

    Add total_calories method to Recipe class

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -8,4 +8,14 @@ class Recipe
   def add_ingredient(ingredient, amount)
     @ingredients_required[ingredient] += amount
   end
+
+  def ingredients
+    @ingredients_required.keys
+  end
+
+  def total_calories
+    ingredients.sum do |ingredient|
+      ingredient.calories.to_i * @ingredients_required[ingredient]
+    end
+  end
 end
