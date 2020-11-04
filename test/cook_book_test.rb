dd33ece6e614e8278810381797c0425895853dee
Catherine Dean <catherinemdean15@gmail.com>
2020-11-04 14:27:13 -0700

    add test_summary to CookBook test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -50,4 +50,33 @@ class CookBookTest < Minitest::Test
     assert_equal "11-04-2020", @cookbook.date
   end
 
+  def test_summary
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Burger")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    expected = [{:name=>"Mac and Cheese",
+                 :details=>{:ingredients=>[{:ingredient=>"Macaroni",
+                                            :amount=>"8 oz"},
+                                           {:ingredient=>"Cheese",
+                                            :amount=>"2 C"}],
+                            :total_calories=>440}},
+                {:name=>"Burger",
+                 :details=>{:ingredients=>[{:ingredient=>"Ground Beef",
+                                            :amount=>"4 oz"},
+                                           {:ingredient=>"Bun",
+                                            :amount=>"100 g"}],
+                            :total_calories=>500}}]
+
+    assert_equal expected, cookbook.summary
+  end
+
 end
