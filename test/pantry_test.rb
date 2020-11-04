9d8fe3d796b993f6af2f9fd4684ce991fc5c071c
Robert Heath <robert.b.heathii@gmail.com>
2020-11-04 14:18:44 -0600

    Test that pantry can stock_check and restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -11,4 +11,20 @@ class PantryTest < Minitest::Test
 
     assert_equal ({}), pantry.stock
   end
+
+  def test_it_can_stock_ingredients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+
+    assert_equal 15, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient2, 7)
+    assert_equal 7, pantry.stock_check(ingredient2)
+  end
 end
