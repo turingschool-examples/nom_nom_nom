fd1dba5b6c89de25b3611e0060f3bec2c1404b37
Grayson Myers <graymyers10@gmail.com>
2020-11-04 13:15:27 -0700

    Fix typos in test causing errors

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -2,15 +2,15 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/ingredient"
 
-class Test < Minitest::Test
+class IngredientTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   def test_it_exists_and_has_attributes
-    assert_instance_of Ingredient @ingredient1
-    assert_instance_of Ingredient @ingredient2
+    assert_instance_of Ingredient, @ingredient1
+    assert_instance_of Ingredient, @ingredient2
 
     assert_equal "Cheese", @ingredient1.name
     assert_equal "oz", @ingredient1.unit
