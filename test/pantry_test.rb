655fc839a7d03552addf2cecd7d2e44b26be13fe
Grayson Myers <graymyers10@gmail.com>
2020-11-04 13:18:03 -0700

    Add existence and attribute test for Pantry class

---
+++ test/pantry_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/pantry"
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists_and_has_attribute
+    assert_instance_of Pantry, @pantry
+
+    assert_equal {}, @pantry.stock
+  end
+end
