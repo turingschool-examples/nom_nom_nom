1390dc2c078a63a0572c8c5cbb32ccbcf6ac6f65
Cydnee Owens <cks410@yahoo.com>
2020-11-04 13:36:34 -0700

    Add total calories test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,10 +5,14 @@ require './lib/ingredient'
 require './lib/pantry'
 
 class RecipeTest < Minitest::Test
+  # Iteration 2
   def setup
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
   end
 
   def test_it_exists_and_has_attributes
@@ -28,5 +32,17 @@ class RecipeTest < Minitest::Test
     assert_equal expected, @recipe1.ingredients_required
     
     assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
+
+  # Iteration 3 
+  def test_it_can_calculate_total_calories 
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, @recipe2.total_calories
   end
 end
