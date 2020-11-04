d90f9e89ced0de16b6393b4240ca6708c53801eb
Yesi Meza <yesi.meza10@gmail.com>
2020-11-04 13:26:19 -0700

    created test for restock and method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -2,7 +2,16 @@ class Pantry
 attr_reader :stock
 
   def initialize
-    @stock = []
+    @stock        = []
+    @stock_amount = 0
+  end
+
+  def stock_check(ingredient)
+      @stock_amount
+  end
+
+  def restock(ingredient, amount)
+    @stock_amount += amount
   end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -16,5 +16,19 @@ class PantryTest < Minitest::Test
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
 
     assert_equal [], pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
   end
+
+  def test_it_can_re_stock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+     pantry.restock(ingredient1, 5)
+     pantry.restock(ingredient1, 10)
+
+    assert_equal 15, pantry.stock_check(ingredient1)
+  end
+
+
 end
