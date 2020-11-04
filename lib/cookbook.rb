40d5b7f5aede3060613f93ae62a63eafffbaabda
Liam Cusack <LCusack8108@gmail.com>
2020-11-04 14:11:42 -0700

    Add cookbook test_cookbook_adds_recipes and test pass

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -1,6 +1,15 @@
 class CookBook
-  attr_reader
+  attr_reader :recipes
 
   def initialize
+    @recipes = []
+  end
+
+  def add_recipe(recipe)
+    @recipes << recipe
+  end
+
+  def recipes
+    @recipes.uniq
   end
 end
--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -1,11 +1,26 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/ingredient'
-require "./lib/pantry"
+require './lib/recipe'
+require "./lib/cookbook"
 
 class CookBookTest < Minitest::Test
   def setup
+
+    @cookbook = CookBook.new
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+  end
+
+  def test_it_exists
+
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_cookbook_addes_recipes
+
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
 end
