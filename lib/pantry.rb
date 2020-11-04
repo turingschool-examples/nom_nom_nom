a416520fe9c47a95e22b1f9607e1d1bc1e9a6f5c
ninesky00 <jiangzho@gmail.com>
2020-11-04 14:18:50 -0600

    Add restock and stock_check method to pass initial test, changed @stock hash default value to 0.

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,6 +1,16 @@
 class Pantry
   attr_reader :stock
   def initialize
-    @stock = {}
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    if @stock[ingredient] ||= amount
+      @stock[ingredient] += amount
+    end
   end
 end
