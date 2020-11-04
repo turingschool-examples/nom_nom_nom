2277dbd3836cbab8da4c3138da206ac49bfa4cbb
Cydnee Owens <cks410@yahoo.com>
2020-11-04 13:27:52 -0700

    Add add_ingredient method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,4 +6,12 @@ class Recipe
     @name                 = name
     @ingredients_required = Hash.new(0)
   end
+
+  def add_ingredient(ingredient, qty)
+    if @ingredients_required[ingredient]
+      @ingredients_required[ingredient] += qty
+    else
+      @ingredients_required[ingredient] = qty
+    end 
+  end
 end
