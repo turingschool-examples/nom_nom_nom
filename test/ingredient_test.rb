869d6330b691274bfc45b1948c02ba4e54b8f285
Yesi Meza <yesi.meza10@gmail.com>
2020-11-04 13:19:40 -0700

    initialized pantry class

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -6,7 +6,8 @@ require './lib/ingredient'
 class IngredientTest < Minitest::Test
   def test_it_exists
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    
     assert_instance_of Ingredient, ingredient1
     assert_equal "Cheese", ingredient1.name
     assert_equal "oz", ingredient1.unit
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,5 +4,17 @@ require './lib/ingredient'
 require './lib/pantry'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
 
-end 
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_can_stock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal [], pantry.stock
+  end
+end
