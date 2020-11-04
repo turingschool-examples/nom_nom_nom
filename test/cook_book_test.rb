bc9a57f2fbe43d2cdd56b5f05474255986d05229
Joseph Budina <joebud3@gmail.com>
2020-11-04 14:24:23 -0700

    Add total calories test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -3,14 +3,18 @@ require 'Minitest/pride'
 require './lib/ingredient'
 require './lib/recipe'
 require './lib/cook_book'
+require './lib/pantry'
 
 class CookBookTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
+    @pantry = Pantry.new
   end
 
   def test_attributes
@@ -26,5 +30,30 @@ class CookBookTest < Minitest::Test
 
     expected = [@recipe1, @recipe2]
     assert_equal expected, @cookbook.recipes
+  end
+
+  def test_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    @recipe2 = Recipe.new("Cheese Burger")
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+
+    @recipe2.add_ingredient(@ingredient3, 4)
+
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    assert_equal 440, @recipe1.total_calories
+
+    assert_equal 677, @recipe2.total_calories
+
+    @cookbook.add_recipe(recipe1)
+
+    @cookbook.add_recipe(recipe2)
+
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected,@cookbook.ingredients
   end
 end
