1f77e1600e6145e367e05e8ec4e692e1d57b3251
Sam Yeo <yeosamuel95@gmail.com>
2020-11-04 13:41:06 -0700

    Add setup for integration test

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -4,6 +4,17 @@ require './lib/pantry'
 require './lib/recipe'
 require './lib/cook_book'
 
-class Test < MiniTest::Test
+class IntegrationTest < MiniTest::Test
+
+    def setup
+        @pantry = Pantry.new
+        @cookbook = CookBook.new
+        @recipe1 = Recipe.new("Mac and Cheese")
+        @recipe2 = Recipe.new("Cheese Burger")
+        @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+        @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+        @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+        @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    end
 
 end
