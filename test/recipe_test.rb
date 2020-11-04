bedafa351d477feaa8af32eeb3d547a97dad5a8e
Phil McCarthy <philmcc2631@gmail.com>
2020-11-04 13:39:36 -0700

    Update earlier tests to not rely on minitest setup method

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,23 +5,44 @@ require './lib/ingredient'
 
 class RecipeTest < Minitest::Test
   def setup
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-    @recipe1 = Recipe.new("Mac and Cheese")
+
   end
 
   def test_it_exists_and_has_attributes
-    assert_equal "Mac and Cheese", @recipe1.name
+    recipe1 = Recipe.new("Mac and Cheese")
+    assert_equal "Mac and Cheese", recipe1.name
     expected = {}
-    assert_equal expected, @recipe1.ingredients_required
+    assert_equal expected, recipe1.ingredients_required
   end
 
   def test_it_can_add_ingredients
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient1, 4)
-    @recipe1.add_ingredient(@ingredient2, 8)
-    expected = {@ingredient1 => 6, @ingredient2 => 8}
-    assert_equal expected, @recipe1.ingredients_required
-    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected = {ingredient1 => 6, ingredient2 => 8}
+    assert_equal expected, recipe1.ingredients_required
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
   end
+
+
 end
+#
+#
+# @pantry = Pantry.new
+# @cookbook = CookBook.new
+# @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+# @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+# @recipe1 = Recipe.new("Mac and Cheese")
+# @recipe1.add_ingredient(@ingredient1, 2)
+# @recipe1.add_ingredient(@ingredient2, 8)
+# @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+# @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+# @recipe2 = Recipe.new("Cheese Burger")
+# @recipe2.add_ingredient(@ingredient1, 2)
+# @recipe2.add_ingredient(@ingredient3, 4)
+# @recipe2.add_ingredient(@ingredient4, 1)
+# assert_equal 440, @recipe1.total_calories
+# assert_equal 675, @recipe2.total_calories
